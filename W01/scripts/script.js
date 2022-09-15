53309d0bc8cb0c26d559012a3891f2b85a80e41c
Rachel Magelsen <mag20002@byui.edu>
2022-09-15 09:19:42 -0600

    being meguca is suffering

---
+++ W01/scripts/script.js
@@ -0,0 +1,14 @@
+// const date = document.querySelector("#date");
+// try {
+// 	const options = {
+// 		day: "numeric",
+// 		month: "numeric",
+// 		year: "numeric"
+// 	};
+// 	date.innerHTML = ` <span>${new Date().toLocaleDateString("en-US", options)}</span>`;
+//     year.innerHTML = document.querySelector(year)
+// } catch (e) {
+// 	alert("Error with code or your browser does not support Locale");
+// }
+let oLastModif = new Date(document.lastModified);
+Date.innerHTML = ``
