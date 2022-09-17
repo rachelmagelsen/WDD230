441c4dcbce3b06b2830ba3ed162e70288e7175a4
Rachel Magelsen <mag20002@byui.edu>
2022-09-16 19:51:37 -0600

    I think I did it??

--- W01/scripts/script.js
+++ W01/scripts/script.js
@@ -1,14 +1,13 @@
-// const date = document.querySelector("#date");
-// try {
-// 	const options = {
-// 		day: "numeric",
-// 		month: "numeric",
-// 		year: "numeric"
-// 	};
-// 	date.innerHTML = ` <span>${new Date().toLocaleDateString("en-US", options)}</span>`;
-//     year.innerHTML = document.querySelector(year)
-// } catch (e) {
-// 	alert("Error with code or your browser does not support Locale");
-// }
-let oLastModif = new Date(document.lastModified);
-Date.innerHTML = ``
+const date = document.querySelector("#date");
+try {
+	const options = {
+		day: "numeric",
+		month: "numeric",
+		year: "numeric"
+	};
+	date.innerHTML = ` Last updated: <span>${new Date().toLocaleDateString("en-US", options)}</span>`;
+} catch (e) {
+	alert("Error with code or your browser does not support Locale");
+}
+const year = document.querySelector("#year");
+year.innerHTML = `&copy<span>${new Date().getFullYear()}</span> | Alicia Meyers | Boston`;
