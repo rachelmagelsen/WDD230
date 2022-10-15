59e0aa79ced381f99320771ac07324daddb409bf
Rachel Magelsen <mag20002@byui.edu>
2022-10-15 16:03:32 -0600

    arefarg

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -27,3 +27,12 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+const day = new Date().getDay();
+
+if (day == 1 || day == 2) {
+    const banner = document.querySelector(".meeting");
+    let para = document.createElement("p");
+    para.textContent = "Come join us for the chamber meet and greet Wednesday at 7:00 pm";
+    banner.appendChild(para);
+}
