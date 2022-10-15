99f87563dd18b554ee8e7f6d50b5111d476eb15d
Rachel Magelsen <mag20002@byui.edu>
2022-10-14 21:33:28 -0600

    here we go

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -27,3 +27,9 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];
+	if(!d.getElementById(id)){js=d.createElement(s);
+	js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';
+	fjs.parentNode.insertBefore(js,fjs);
+	}}(document,'script','weatherwidget-io-js');
