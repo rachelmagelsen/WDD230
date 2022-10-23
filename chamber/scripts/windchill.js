a98b6bacc9e3bd39efa008437107b51cd5cbf27c
Rachel Magelsen <mag20002@byui.edu>
2022-10-22 22:06:00 -0600

    uktfjfhgmc

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,13 +1,13 @@
-const speed = parseInt(document.getElementById("speed").innerHTML);
-const temp = parseInt(document.getElementById("temp").innerHTML);
+const speed = parseInt(document.getElementById("speed").textContent);
+const temp = parseInt(document.getElementById("temp").textContent);
 
-if (t <= 50 && s > 3.0) {
+if (temp <= 50 && speed > 3.0) {
   const windchill =
     35.74 +
     0.6215 * temp -
     35.75 * Math.pow(speed, 0.16) +
-    0.42756 * t * Math.pow(speed, 0.16);
-  document.getElementById("windchill").innerHTML =
+    0.42756 * temp * Math.pow(speed, 0.16);
+    document.getElementById("windchill").innerHTML =
     Math.round(windchill);
 } else {
   document.getElementById("windchill").innerHTML = "N/A";
