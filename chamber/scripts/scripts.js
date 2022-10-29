6e1f5210a21b3058c17ed3df02fc661519cbbbe4
Rachel Magelsen <mag20002@byui.edu>
2022-10-28 22:13:27 -0600

    asdfjklbsFHBAKR

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -36,3 +36,30 @@ if (day == 1 || day == 2) {
     para.textContent = "Come join us for the chamber meet and greet Wednesday at 7:00 pm";
     banner.appendChild(para);
 }
+
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}; 
---
+++ chamber/scripts/visits.js
@@ -0,0 +1,10 @@
+const visits = document.getElementById("visits");
+let lastVisit = Number(window.localStorage.getItem("visit-date"));
+let today = Date.now()
+
+let difference = (today - lastVisit);
+let time = (difference / 86,400,000)
+rounded = Math.ceil(time)
+
+localStorage.setItem("lastVisit", today);
+visits.textContent = rounded;
