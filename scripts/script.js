ea614bab30ff0c0a7d0433f4cf73d9338a408b59
Rachel Magelsen <mag20002@byui.edu>
2022-10-28 19:15:55 -0600

    javascript is not my friend :,)

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,16 @@
-const date = document.querySelector("#lastModified");
-try {
-	const options = {
-		day: "numeric",
-		month: "numeric",
-		year: "numeric"
-	};
-	date.innerHTML = `Last updated: <span>${new Date().toLocaleDateString("en-US", options)}</span>`;
-} catch (e) {
-	alert("Error with code or your browser does not support Locale");
-}
-const year = document.querySelector("#year");
-year.innerHTML = `&copy<span>${new Date().getFullYear()}</span> | Rachel Magelsen | Washington`;
+// const date = document.querySelector("#lastModified");
+// try {
+// 	const options = {
+// 		day: "numeric",
+// 		month: "numeric",
+// 		year: "numeric"
+// 	};
+// 	date.innerHTML = `Last updated: <span>${new Date().toLocaleDateString("en-US", options)}</span>`;
+// } catch (e) {
+// 	alert("Error with code or your browser does not support Locale");
+// }
+// const year = document.querySelector("#year");
+// year.innerHTML = `&copy<span>${new Date().getFullYear()}</span> | Rachel Magelsen | Washington`;
+
+const options = {weekday: "long", day: "numeric", month: "long", year: "numeric"};
+document.getElementById("currentdate").textContent = newDate().toLocalDateString("en-US", options);
