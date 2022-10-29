85b60c33e64d43fc6cc7180c80903a2e01fc9093
Rachel Magelsen <mag20002@byui.edu>
2022-10-28 22:14:10 -0600

    week07

--- chamber/scripts/visits.js
+++ chamber/scripts/visits.js
@@ -1,10 +1,8 @@
 const visits = document.getElementById("visits");
 let lastVisit = Number(window.localStorage.getItem("visit-date"));
 let today = Date.now()
-
 let difference = (today - lastVisit);
 let time = (difference / 86,400,000)
 rounded = Math.ceil(time)
-
 localStorage.setItem("lastVisit", today);
 visits.textContent = rounded;
