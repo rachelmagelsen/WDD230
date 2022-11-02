d92952454b3d994eb8181bf183804674c1254e33
Rachel Magelsen <mag20002@byui.edu>
2022-11-01 21:10:12 -0600

    lesson8

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,2 @@
-// const date = document.querySelector("#lastModified");
-// try {
-// 	const options = {
-// 		day: "numeric",
-// 		month: "numeric",
-// 		year: "numeric"
-// 	};
-// 	date.innerHTML = `Last updated: <span>${new Date().toLocaleDateString("en-US", options)}</span>`;
-// } catch (e) {
-// 	alert("Error with code or your browser does not support Locale");
-// }
-// const year = document.querySelector("#year");
-// year.innerHTML = `&copy<span>${new Date().getFullYear()}</span> | Rachel Magelsen | Washington`;
-
-const options = {weekday: "long", day: "numeric", month: "long", year: "numeric"};
-document.getElementById("currentdate").textContent = newDate().toLocalDateString("en-US", options);
+let lastModified = document.lastModified;
+document.getElementById('modifiedDate').innerHTML = lastModified;
