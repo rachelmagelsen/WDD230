5f8232cc85d2aacf621bb323fa3074295f94b4d0
Rachel Magelsen <mag20002@byui.edu>
2022-11-09 21:59:44 -0700

    ????

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const requestURL = 'https://rachelmagelsen.github.io/WDD230/chamber/data.json';
 const cards = document.querySelector('.cards');
 
 fetch(requestURL) 
@@ -6,34 +6,32 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    const prophets = jsonObject["prophets"];
-    prophets.forEach(displayProphets);
+    const businesses = jsonObject["businesses"];
+    businesses.forEach(displayBusinesses);
   });
 
-function displayProphets(prophet) {
-  // Create elements to add to the document
+function displayBusinesses(business) {
   let card = document.createElement('section');
   let h2 = document.createElement('h2');
-  let portrait = document.createElement('img');
-  let birthDate = document.createElement('h3');
-  let birthPlace = document.createElement('h3');
+  let logo = document.createElement('img');
+  let address = document.createElement('p');
+  let phone = document.createElement('p');
+  let website = document.createElement('p');
 
-  // Change the textContent property of the h2 element to contain the prophet's full name
-  h2.textContent = `${prophet.name} ${prophet.lastname}`;
-  birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
-  birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+  h2.textContent = `${business.name}`;
+  address.textContent = `${business.address}`;
+  phone.textContent = `${business.phone}`;
+  website.textContent = `${business.webite}`;
 
-  // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-  portrait.setAttribute('src', prophet.imageurl);
-  portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname} - ${prophet.order}th Latter-Day Prophet`);
-  portrait.setAttribute('loading', 'lazy');
+  logo.setAttribute('src', business.imageurl);
+  logo.setAttribute('alt', `logo`);
+  logo.setAttribute('loading', 'lazy');
 
-  // Add/append the section(card) with the h2 element
+  card.appendChild(logo);
   card.appendChild(h2);
-  card.appendChild(birthDate);
-  card.appendChild(birthPlace);
-  card.appendChild(portrait);
+  card.appendChild(address);
+  card.appendChild(phone);
+  card.appendChild(website);
 
-  // Add/append the existing HTML div with the cards class with the section(card)
   document.querySelector('div.cards').appendChild(card);
 }
